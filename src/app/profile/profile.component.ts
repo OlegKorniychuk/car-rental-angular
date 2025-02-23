05a3f29db6fc64d50450160a1ae150bd6a635735
OlegKorniychuk <kornoleg576@gmail.com>
2025-02-23 22:07:01 +0200

    feat: basic profile ready

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../layout/header/header.component';
-import { FooterComponent } from '../layout/footer/footer.component';
+import { Component, OnInit } from '@angular/core';
+import { UserService, User } from '../auth/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,6 +7,29 @@ import { FooterComponent } from '../layout/footer/footer.component';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  public userData: User | null = null;
+  public isLoading: boolean = true;
+  public error: string | null = null;
 
+  constructor(
+    private readonly userService: UserService,
+  ) { }
+
+  ngOnInit(): void {
+    this.fetchClient();
+  }
+
+  public fetchClient(): void {
+    this.userService.getCurrentUser().subscribe({
+      next: (user) => {
+        this.userData = user;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load profile info.';
+        this.isLoading = false;
+      }
+    })
+  }
 }
